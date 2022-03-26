45bf0995b6fe68124c01d373b35955bbc1af3531
Oshamaru <callMeSam808@gmail.com>
2022-03-25 14:27:50 -1000

    Add reset button and prompt for new grid size

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-const container = document.querySelector('#container');
-
 function createGrid(number = 16) {
+  const container = document.querySelector('#container');
   for (y = 1; y <= number; y++) {
     let row = document.createElement('div');
     row.id = 'row';
@@ -11,14 +10,30 @@ function createGrid(number = 16) {
       row.appendChild(gridDiv);
     }
   }
+  const boxes = document.querySelectorAll('#box');
+  for (const box of boxes) {
+    box.addEventListener('mouseover', function(e) {
+    e.target.classList.add('hover');
+  });
+}
 }
 
 createGrid();
 
-const boxes = document.querySelectorAll('#box');
-console.log(boxes);
-for (const box of boxes) {
-  box.addEventListener('mouseover', function(e) {
-    e.target.classList.add('hover');
-  });
+const button = document.querySelector('#reset');
+button.addEventListener('click', resetSketch);
+
+function resetSketch() {
+  const body = document.querySelector('body');
+  const container = document.querySelector('#container');
+  container.remove();
+  let newContainer = document.createElement('div');
+  newContainer.id = 'container';
+  body.insertBefore(newContainer, body.lastChild);
+  let number = prompt("How big would you like your grid? (Max value is 100)");
+  if (!isNaN(number) && number > 0 && number < 101) {
+    createGrid(number);
+  } else {
+    number = prompt("Please enter a valid number (1-100)");
+  }
 }
